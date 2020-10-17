ab43b098d1b54396f4b88ed55eda93654ea48404
DESKTOP-5333BH6\Lenovo <rizki@udacoding.com>
2020-10-17 10:38:19 +0700

    disable debug flag

--- codingtalk38_provider_bloc/lib/main.dart
+++ codingtalk38_provider_bloc/lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: HomePage(), debugShowCheckedModeBanner: false,
     );
   }
 }
